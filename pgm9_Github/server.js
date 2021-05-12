90041b7260db5259841b627af3696debd337f54d
vinay-nb <vinaybhathg11@gmail.com>
2021-05-12 15:42:24 +0530

    updated examples

--- pgm9_Github/server.js
+++ pgm9_Github/server.js
@@ -5,33 +5,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use('/images', express.static(__dirname + '/public/images'));
-// app.get('/',function(req,resp)
-// {
-// 	resp.sendFile(__dirname+"/"+"public/index.html")
-// });
-// app.post('/login',function(req,resp)
-// {
-// 	if(req.body.username=='admin'&& req.body.password=='admin123')
-// 	{
-// 		console.log('Logged in successfully');
-// 		resp.sendFile(__dirname+"/"+"public/success.html")
-// 	}
-// 	else
-// 	{
-// 		console.log('invalid credentials');
-// 		resp.sendFile(__dirname+"/"+"public/error.html")
-// 	}
-// });
 
 //9th program git proile display
-app.get('/git',function (req, resp){
+app.get('/',function (req, resp){
 	resp.sendFile(__dirname+"/"+"public/gitppro.html")
 });
 
-//for 10th program
-app.get('/stock_data',function (req, resp){
-	resp.sendFile(__dirname+"/"+"public/stock.html")
-});
 
 app.listen(9500,function(){
 	console.log('Listening at port 9500');
